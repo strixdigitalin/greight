7df8e3504f52a9f790af971b9ba26b3fbffddf0c
lakheraakshay <akshay.lakhera@easystepin.com>
2023-04-06 02:29:19 +0530

    changes done

--- Schema/Contactus.schema.js
+++ Schema/Contactus.schema.js
@@ -5,7 +5,7 @@ const Schema = new mongoose.Schema(
     fullName: { type: String, required: [true, "Full Name is required"] },
     email: { type: String, required: [true, "Email is required"] },
     message: { type: String, required: [true, "Message is required"] },
-    contact: { type: Number, required: [true, "Contact is required"] },
+    // contact: { type: Number, required: [true, "Contact is required"] },
   },
   { timestamps: true }
 );
